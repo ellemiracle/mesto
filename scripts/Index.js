4fe6ac7c4d1abdae029157f29b6d26626f850cd7
Elmira Akhmetzyanova <Adgihan89@yandex.ru>
2022-02-23 19:03:52 +0300

    Изменение блоков и попапа

--- scripts/Index.js
+++ scripts/Index.js
@@ -1,47 +1,24 @@
-const popupElement = document.querySelector('.popup'); //переменная для всего popup
-const editProfile = document.querySelector('.edit-profile'); //переменная для открытия редактирования профиля
-const closeButton = popupElement.querySelector('.popup__close-button'); //переменная для закрытия редактирования
-const saveButton = document.querySelector('.popup__save-button'); //переменная для кнопки сохранить
-const userName = document.querySelector('.profile__user-name'); //переменная имени профиля на странице
-const userJop = document.querySelector('.profile__user-jop'); //переменная работы на странице
+const popup = document.querySelector('.popup');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popupExitButton = popup.querySelector('.popup__exit-button');
+const formElement = document.querySelector('.popup__form');
+const popupFieldName = popup.querySelector('.popup__field_name');
+const popupFieldDescription = popup.querySelector('.popup__field_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
 
-	let nameInput = document.querySelector('.popup__user-name');
-	let jobInput = document.querySelector('.popup__user-jop');
-
-function openPopup() {
-	popupElement.classList.remove('popup_opened');
+const popupToggle = function() {
+  popup.classList.toggle('popup_opened');
 }
 
-function closePopup() {
-	popupElement.classList.add('popup_opened');
-}
+profileEditButton.addEventListener('click', popupToggle);
+popupExitButton.addEventListener('click', popupToggle);
 
-editProfile.addEventListener('click', openPopup)
-closeButton.addEventListener('click', closePopup)
-
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
-nameInput.value = userName.textContent;
-jobInput.value = userJop.textContent;
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
-	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
-	// Находим поля формы в DOM
-
-	// Получите значение полей из свойства value
-	userName.textContent = nameInput.value;
-	userJop.textContent = jobInput.value;
-
-	closePopup();
-	// Выберите элементы, куда должны быть вставлены значения полей
-
-	// Вставьте новые значения с помощью textContent
+  evt.preventDefault();
+  profileName.textContent = popupFieldName.value;
+  profileDescription.textContent = popupFieldDescription.value;
+  popupToggle();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);
