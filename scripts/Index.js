e37239e2043587a7e181c3a3033816816ec72989
Elmira Akhmetzyanova <Adgihan89@yandex.ru>
2022-02-27 17:45:15 +0300

    после 2 ревью

--- scripts/Index.js
+++ scripts/Index.js
@@ -2,23 +2,40 @@ const popup = document.querySelector('.popup'); //переменная для в
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupExitButton = popup.querySelector('.popup__exit-button');
 const formElement = document.querySelector('.popup__form');
-const popupFieldName = popup.querySelector('.popup__field_name');
-const popupFieldDescription = popup.querySelector('.popup__field_description');
+const popupFieldName = popup.querySelector('.popup__field__input_name');
+const popupFieldDescription = popup.querySelector('.popup__field__input_description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
-const popupToggle = function() {
-  popup.classList.toggle('popup_opened');
+// const popupToggle = function() {
+//   popup.classList.toggle('popup_opened');
+// }
+
+
+
+// profileEditButton.addEventListener('click', popupToggle);
+// popupExitButton.addEventListener('click', popupToggle);
+
+const popupOpen = function() {
+  popup.classList.add('popup_opened');
+  
+  //2 метода ниже добавляют в форму попапа содержимое из профиля
+  popupFieldName.value = profileName.innerHTML;
+  popupFieldDescription.value = profileDescription.innerHTML;
 }
 
-profileEditButton.addEventListener('click', popupToggle);
-popupExitButton.addEventListener('click', popupToggle);
+const popupClose = function() {
+  popup.classList.remove('popup_opened')
+}
+
+profileEditButton.addEventListener('click', popupOpen);
+popupExitButton.addEventListener('click', popupClose);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = popupFieldName.value;
   profileDescription.textContent = popupFieldDescription.value;
-  popupToggle();
+  popupClose();
 }
 
 // Прикрепляем обработчик к форме:
